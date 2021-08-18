5de9ff499b8ca4f1077e7ed39d575d0c1eb70484
Andra Satria Pratama <andrasatriapratama@gmail.com>
2021-08-18 11:56:16 +0700

    penambahan activity huruf a

--- app/src/main/java/com/android/hiragana/MainActivity.java
+++ app/src/main/java/com/android/hiragana/MainActivity.java
@@ -2,13 +2,25 @@ package com.android.hiragana;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView txt_a;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        txt_a = findViewById(R.id.huruf_a);
+        txt_a.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, Menu1.class));
+            }
+        });
     }
 }
