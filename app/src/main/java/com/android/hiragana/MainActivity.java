c41a20f6e82c31c11b4251c1b97a27978995babe
Andra Satria Pratama <andrasatriapratama@gmail.com>
2021-08-21 18:06:51 +0700

    perubahan tataletak menu

--- app/src/main/java/com/android/hiragana/MainActivity.java
+++ app/src/main/java/com/android/hiragana/MainActivity.java
@@ -8,51 +8,18 @@ import android.view.View;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
-    TextView txt_a, txt_i, txt_u, txt_e, txt_o;
-
+    TextView txt_gojuuon;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        txt_a = findViewById(R.id.huruf_a);
-        txt_i = findViewById(R.id.huruf_i);
-        txt_u = findViewById(R.id.huruf_u);
-        txt_e = findViewById(R.id.huruf_e);
-        txt_o = findViewById(R.id.huruf_o);
 
-        txt_a.setOnClickListener(new View.OnClickListener() {
+        txt_gojuuon = findViewById(R.id.txt_gojuuon);
+
+        txt_gojuuon.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                startActivity(new Intent(MainActivity.this, HurufA.class));
-            }
-        });
-
-        txt_i.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                startActivity(new Intent(MainActivity.this, HurufI.class));
-            }
-        });
-
-        txt_u.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                startActivity(new Intent(MainActivity.this, HurufU.class));
-            }
-        });
-
-        txt_e.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                startActivity(new Intent(MainActivity.this, HurufE.class));
-            }
-        });
-
-        txt_o.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                startActivity(new Intent(MainActivity.this, HurufO.class));
+                startActivity(new Intent(MainActivity.this, Gojuuon.class));
             }
         });
     }
